bbaa4fca08fa8b80dbb8f455c3c1ebe96e8e5a34
Alejandro Marco <Alejandro Marco>
2017-02-23 11:17:24 +0100

    AHora la actividad principal es capaz de llamar a la actividad de la calculadora.

--- DosActividades/app/src/main/java/com/example/android/dosactividades/MainActivity.java
+++ DosActividades/app/src/main/java/com/example/android/dosactividades/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.dosactividades;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
+
+    /**
+     * Escuchamos al botón calculadora
+     */
+    public void botonCalculadora(View view){
+        irCalculadora();
+    }
+
+    /**
+      * Con este método llamaremos a la aplicación de la calculadora.
+     */
+    private void irCalculadora(){
+
+        Intent intencion=new Intent(this,CalcActivity.class);
+        super.startActivityForResult(intencion, 1234);
+
+        //Evitamos crash si no hay respuesta.
+        if (intencion.resolveActivity(getPackageManager())!= null){
+            startActivity(intencion);
+        }
+    }
+
 }

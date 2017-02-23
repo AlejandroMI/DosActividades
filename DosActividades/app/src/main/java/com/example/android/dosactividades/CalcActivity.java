dd43aa2aa27fddf13937858b991f8adf9944fc74
Alejandro Marco <Alejandro Marco>
2017-02-23 10:45:52 +0100

    Hemos creado la segunda actividad definiendo el diseño básico.

---
+++ DosActividades/app/src/main/java/com/example/android/dosactividades/CalcActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.dosactividades;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alejandro on 23/02/2017.
+ */
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}

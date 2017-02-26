8aa5cd83d9a08a24e9de96ef3a761af4e9717b89
Alejandro Marco <Alejandro Marco>
2017-02-26 17:42:30 +0100

    La calculadora es funcional. Queda adaptarla de forma que llame al main y pueda compartir el resultado.

--- DosActividades/app/src/main/java/com/example/android/dosactividades/CalcActivity.java
+++ DosActividades/app/src/main/java/com/example/android/dosactividades/CalcActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import static android.R.attr.value;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static android.os.Build.VERSION_CODES.M;
 
 /**
  * Created by Alejandro on 23/02/2017.
@@ -13,8 +15,10 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class CalcActivity extends AppCompatActivity {
 
-    private CharSequence textoD = ""; //Para poder guardar el texto del display
-    private int resultado; //Guardaremos el resultado final.
+    private CharSequence textoD = ""; //Para poder guardar el texto del display, lo
+    //utilizaremos para realizar el cálculo final.
+    private int resultado=0; //Guardaremos el resultado final.
+    public int resultadoC=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,55 +26,57 @@ public class CalcActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calc);
     }
 
-
-
+    /**
+     * Con el siguiente metodo leeremos la entrada del teclado y llamaremos a otros
+     * */
     public void leerTeclado(View view) {
 
         TextView textoTeclado = (TextView) findViewById(view.getId());
-        display(textoTeclado);
 
-        switch (view.getId()) {
+        if (view.getId()!=R.id.tigual) {
 
-            case R.id.t7: /** Se ha pulsado la tecla 7 */
-                break;
-            case R.id.t8: /** Se ha pulsado la tecla 8 */
-                break;
-            case R.id.t9: /** Se ha pulsado la tecla 9 */
-                break;
-            case R.id.t4: /** Se ha pulsado la tecla 4 */
-                break;
-            case R.id.t5: /** Se ha pulsado la tecla 5 */
-                break;
-            case R.id.t6: /** Se ha pulsado la tecla 6 */
-                break;
-            case R.id.t1: /** Se ha pulsado la tecla 1 */
-                break;
-            case R.id.t2: /** Se ha pulsado la tecla 2 */
-                break;
-            case R.id.t3: /** Se ha pulsado la tecla 3 */
-                break;
-            case R.id.tsuma: /** Se ha pulsado la tecla + */
-                break;
-            case R.id.t0: /** Se ha pulsado la tecla 0 */
-                break;
-            case R.id.tigual: /** Se ha pulsado la tecla = */
-                break;
+            display(textoTeclado);
+
+        }else{
+
+            calculo();
         }
     }
 
+    /**
+     * Representamos la operación en el display mediante la concatenación de la entrada.
+     * */
+
    private void display(TextView text) {
 
-        TextView textoDisplay = (TextView)findViewById(R.id.textoDisplay); //Puntero al display
-        CharSequence textoT = text.getText(); //Cogemos el texto de la tecla pulsada
-        textoD = textoD.toString() +textoT; //Concatenamos el texto del display con el de la tecla
+        TextView textoDisplay = (TextView)findViewById(R.id.textoDisplay); //Puntero al view display
+        CharSequence textoT = text.getText(); //Cogemos el texto de la tecla pulsada. El método
+                                              //leerTeclado nos habra pasado la view correspond.
+        textoD = textoD.toString()+textoT; //Concatenamos el texto del display con el de la tecla
         textoDisplay.setText(textoD); //Cambiamos el texto al nuevo string
 
    }
 
 
-    private void calculo(int entrada){
+    private void calculo(){
 
+            TextView textoDisplay = (TextView)findViewById(R.id.textoDisplay); //View display
+            String[] resultArray= textoD.toString().split("[+]"); //obtenemos todos los sumandos
+            //Los sumamos todos
+            for (int i=0; i<=resultArray.length-1; i++){
 
+                resultado= resultado +Integer.parseInt(resultArray[i]);
+            }
+
+            //Representamos el resultado en la pantalla.
+            String result = String.valueOf(resultado);
+            textoDisplay.setText(result);
+
+            //Reseteamos el display y el resultado para poder hacer una nueva suma
+            textoD="";
+            resultadoC=resultado; //Guardamos el resultado para compartirlo.
+            resultado=0;
     }
+
 
 }
